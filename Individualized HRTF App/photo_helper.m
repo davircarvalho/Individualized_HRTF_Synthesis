3e2ff690b5b23c47ab2757a4400d4ac6771c953f
Elephant ~ <r.davicarvalho@gmail.com>
2021-01-21 12:16:27 -0300

    photo scale color

--- Individualized HRTF App/photo_helper.m
+++ Individualized HRTF App/photo_helper.m
@@ -85,13 +85,13 @@ mag = hypot(diffPos(1),diffPos(2));
 
 % Choose a color from the color map based on the length of the line. The
 % line changes color as it gets longer or shorter.
-color = src.UserData.Colormap(ceil(64*(mag/src.UserData.MaxValue)),:);
+% color = src.UserData.Colormap(ceil(64*(mag/src.UserData.MaxValue)),:);
 
 % Apply the scale factor to line length to calibrate the measurements.
 mag = mag*src.UserData.ScaleFactor;
 
 % Update the label.
-set(src,'Label',[num2str(mag,'%30.1f') ' ' src.UserData.Units],'Color',color);
+set(src,'Label',[num2str(mag,'%30.1f') ' ' src.UserData.Units],'Color','k');
 end
 
 
