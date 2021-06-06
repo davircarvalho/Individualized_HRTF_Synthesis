d3ec5755fb1f4a2a52c2f025a4d6ca379949ff78
Elephant ~ <r.davicarvalho@gmail.com>
2021-06-06 14:45:42 -0300

    added axis labels

--- Tests/test_ITD_synthesis.m
+++ Tests/test_ITD_synthesis.m
@@ -26,7 +26,7 @@ itd_sphe = itd_synthesis(width, depth, pos, fs, method, 'time');
 method = 'adapt';
 itd_adpt = itd_synthesis(width, depth, pos, fs, method, 'time');
 
-itd_real = SOFAgetITD(Obj, 'time', 'thr', 30);
+itd_real = SOFAgetITD(Obj, 'time', 'thr', 20);
 
 
 sum(abs(itd_real - itd_sphe))
@@ -38,4 +38,6 @@ plot(itd_real, 'linewidth', 1.5); hold on
 plot(itd_sphe);
 plot(itd_adpt); hold off
 legend('Measured', 'Spheric', 'Adapt', 'location', 'best')
-
+ylabel('Time (s)')
+xlabel('Position index')
+axis tight
