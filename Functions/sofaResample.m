7b872fb41902c97dabb5abbcf2c601eb11469267
Elephant ~ <r.davicarvalho@gmail.com>
2021-06-11 12:51:37 -0300

    slightly better resampling method

--- Functions/sofaResample.m
+++ Functions/sofaResample.m
@@ -1,52 +1,68 @@
-function Obj = sofaResample(Obj, Fs, Nintp)
+function Obj = sofaResample(Obj, Fs)
 % Muda resolução aparente de objeto SOFA para valor especificado
-% e faz zero padding para 2^nextpow2(N)
 
-% Davi R. Carvalho @UFSM - Engenharia Acustica - Setembro/2020
+% Davi R. Carvalho @UFSM - Engenharia Acustica - Junho/2021
 
 %   Input Parameters:
 %    Obj:        Objeto de HRTFs SOFA a ser modificado 
 %    Fs:         Taxa de amostragem Objetivo
-%    Nint (opcional):       Comprimento do vetor na saida, (adiciona zeros no final)
 
 %   Output Parameters:
-%     Obj_out:   Objeto de HRTFs SOFA com a taxa de amostragem Fs
+%     Obj:   Objeto de HRTFs SOFA com a taxa de amostragem Fs
 %
-% Matlab 2020a
+% Matlab R2020b
 %% Resample
 Fs_sofa = Obj.Data.SamplingRate;
-N = ceil((Fs/Fs_sofa) * size(Obj.Data.IR, 3)); % length after resample
-if nargin<3 || Nintp<N
-    Nintp = N;
-end
-zpad = zeros((Nintp - N), 1);
 
-
-%% options
-tx = (0:Obj.API.N-1)/Fs_sofa;
-[p,q] = rat(Fs / Fs_sofa);
-% normFc = .98 / max(p,q);
-% order = 256 * max(p,q);
-% beta = 12;
-% %%% Cria um filtro via Least-square linear-phase FIR filter design
-% lpFilt = firls(order, [0 normFc normFc 1],[1 1 0 0]);
-% lpFilt = lpFilt .* kaiser(order+1,beta)';
-% lpFilt = lpFilt / sum(lpFilt);
-% % multiply by p
-% lpFilt = p * lpFilt;
-% Actual Resample
-for k = 1:size(Obj.Data.IR, 1)
-    for l = 1:size(Obj.Data.IR, 2)
-%         IRpre(k, l, :) = resample(Obj.Data.IR(k, l, :),p,q,lpFilt);
-        IRpre(k, l, :) = resample(squeeze(Obj.Data.IR(k, l, :)), ...
-                                          tx, Fs, p,q, 'spline');
-        IR(k, l, :) = [squeeze(IRpre(k, l, :)); zpad];
-    end 
+%% Check if upsampling is necessary first
+factor = 4;
+if Fs>Fs_sofa && Fs/Fs_sofa<factor || ... % do upsampling first 
+        Fs<Fs_sofa && Fs_sofa/Fs<factor
+    Fs_up = Fs_sofa*factor;
+    IR_upsample = resample_this(Obj.Data.IR, Fs_sofa, Fs_up);
+    IR = resample_this(IR_upsample, Fs_up, Fs);  
+else % just do the resample already
+    IR = resample_this(Obj.Data.IR, Fs_sofa, Fs);
 end
+
 %% Output
-% norm = max(abs(Obj.Data.IR));
 Obj.Data.IR = IR;
 % update sampling rate
 Obj.Data.SamplingRate = Fs;
 Obj = SOFAupdateDimensions(Obj);
 end
+
+
+%--------------------------------------------------------------------------
+function IR = resample_this(X, Fs_in, Fs_out)
+    [p,q] = rat(Fs_out / Fs_in, 0.0001);
+    normFc = .965 / max(p,q);
+    order = 256 * max(p,q);
+    beta = 12;
+    %%% Cria um filtro via Least-square linear-phase FIR filter design
+    lpFilt = firls(order, [0 normFc normFc 1],[1 1 0 0]);
+    lpFilt = lpFilt .* kaiser(order+1,beta)';
+    lpFilt = lpFilt / sum(lpFilt);
+    lpFilt = p * lpFilt;
+
+    % Initializar matriz
+    N_pos = size(X, 1);
+    N_ch = size(X, 2);
+    N_samples = ceil((Fs_out/Fs_in) * size(X, 3)); % length after resample
+    IR=zeros(N_pos, N_ch, N_samples);
+
+    % Actual Resample
+    for k = 1:N_pos
+        for l = 1:N_ch
+            IR(k, l, :) = resample(X(k,l,:), p, q, lpFilt);
+        end 
+    end
+    IR = IR.* q/p; % check scaling
+
+    % make sure signal length is not odd
+    if rem(size(IR,3), 2) ~= 0
+       IR(:,:,end) = []; 
+    end
+end
+
+
--- Tests/test_resample.m
+++ Tests/test_resample.m
@@ -1,6 +1,7 @@
 clear all; clc
 addpath([pwd, '\..\Functions']);
 local = [pwd, '\..\Datasets\3D3A\Public-Data\Subject17\Subject17_HRIRs.sofa'];
+% local = [pwd, '\..\Datasets\ARI\hrtf b_nh8.sofa'];
 Obj = SOFAload(local);
 
 
@@ -19,17 +20,18 @@ IR1 = shiftdim(Obj.Data.IR, 2);
 IR2 = shiftdim(Obj2.Data.IR, 2);
 
 
-%% plot
+% plot
+figure
 subplot(211)
 tx1 = (0:N1-1)/fs1; 
 tx2 = (0:N2-1)/fs2; 
-ir1 = IR1(:,1,1) ./ max(abs(IR1(:,1,1)));
-ir2 = IR2(:,1,1) ./ max(abs(IR2(:,1,1)));
+ir1 = IR1(:,1,1);       
+ir2 = IR2(:,1,1);
 
 plot(tx1, ir1); hold on 
 plot(tx2, ir2); hold on 
 axis tight
-legend('original', 'resample', 'location', 'best')
+legend('Original', 'Resample', 'location', 'best')
 xlabel('tempo (s)')
 subplot(212)
 semilogx(f1, db(abs(fft(ir1, N1)))); hold on
@@ -37,3 +39,62 @@ semilogx(f2, db(abs(fft(ir2, N2))));
 xlim([0, 2e4])
 legend('original', 'resample', 'location', 'best')
 xlabel('frequencia (Hz)')
+
+
+
+
+
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fs1 = 96000;
+t1 = 0:1/fs1:1;
+x = chirp(t1, 0, 1, fs1/2, 'quadratic');
+figure
+spectrogram(x,kaiser(256,15),220,412,fs1,'yaxis')
+
+%%
+fs2 = 44100;
+[p,q] = rat(fs2/fs1);
+y = resample(x,p,q);
+spectrogram(y,kaiser(256,15),220,412,fs2,'yaxis')
+
+%%
+fs2 = 44100;
+t2 = 0:1/fs2:1;
+
+y = resample_ya(x, fs1,fs2);
+figure
+spectrogram(y,kaiser(256,15),220,412,fs2,'yaxis')
+
+
+
+function IR = resample_ya(x, Fs_in, Fs_out, Nintp)
+N = ceil((Fs_out/Fs_in) * size(x, 3)); % length after resample
+if nargin<4 || Nintp<N
+    Nintp = N;
+end
+zpad = zeros((Nintp - N), 1);
+
+N_sig = length(x);
+%% options
+tx = (0:N_sig-1)/Fs_in;
+[p,q] = rat(Fs_out / Fs_in, 0.0001);
+normFc = .98 / max(p,q);
+order = 512 * max(p,q);
+beta = 12;
+%%% Cria um filtro via Least-square linear-phase FIR filter design
+lpFilt = firls(order, [0 normFc normFc 1],[1 1 0 0]);
+lpFilt = lpFilt .* kaiser(order+1,beta)';
+lpFilt = lpFilt / sum(lpFilt);
+% multiply by p
+lpFilt = p * lpFilt;
+% Actual Resample
+
+IRpre = resample(x,p,q,lpFilt);
+% IRpre = resample(x, tx, Fs_out, p,q, 'spline');
+IR = [IRpre; zpad];
+%% Output
+% norm = max(abs(Obj.Data.IR));
+IR = IR.* q/p;
+end
+
