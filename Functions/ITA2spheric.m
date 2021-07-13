1e51b2126070cd90486995abf2190c3fc8cc8265
Elephant ~ <r.davicarvalho@gmail.com>
2021-07-13 20:36:29 -0300

    ITA coordinates correction

---
+++ Functions/ITA2spheric.m
@@ -0,0 +1,20 @@
+function Obj = ITA2spheric(Obj)
+
+for l = 1:length(Obj.SourcePosition)
+    x = Obj.SourcePosition(l, 1);  
+    y = Obj.SourcePosition(l, 2); 
+    z = Obj.SourcePosition(l, 3);
+    % new coordinates
+    [az,elev,r] = cart2sph(x,y,z);
+    azi=rad2deg(az); elev=rad2deg(elev);
+    [azi,ele]   = nav2sph(azi,elev);
+    % update coordinates
+    Obj.SourcePosition(l, 1) = azi;
+    Obj.SourcePosition(l, 2) = ele; 
+    Obj.SourcePosition(l, 3) = round(r);
+    % more metadata
+    Obj.SourcePosition_Type = 'spherical';
+    Obj.SourcePosition_Units = 'degree, degree, meter';              
+end  
+
+end
